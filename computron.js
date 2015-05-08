2250735fb27bd29ff8540a259e96f1d3e80b70c3
Remco Veldkamp <remcoder@gmail.com>
2015-05-09 00:37:20 +0200

    fixed instanceof

--- computron.js
+++ computron.js
@@ -8,11 +8,8 @@ function Computron(f) {
         value.set( f(comp) );
     });
 
-    // don't just return the underlying reactive var b/c we don't want it to be modified.
-    // so, create a new immutable object to pass around
-    return {
-        get: function() { return value.get(); }
-    };
+    // only expose get(). immutability FTW
+    this.get = function() { return value.get(); }
 }
 
 this.Computron = Computron;
